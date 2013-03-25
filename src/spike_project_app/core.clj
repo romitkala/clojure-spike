3bc920892f40e16138006fc2103213fda8a92329
Romit Kala <rkala@thoughtworks.com>
2013-03-25 15:47:22 +1100

    parse csv into map

--- src/spike_project_app/core.clj
+++ src/spike_project_app/core.clj
@@ -2,36 +2,19 @@
   (:require [clojure-csv.core :as csv])
   (:gen-class))
 
-(defn parse-row [row]
-    (let [v (first (csv/parse-csv row))]
-      (zipmap [:OrdrTyp :DstId :FndPrvId :SwtchRef :OrdrRef :FndIdType :FndId :FndNm :PrdtCd :Unit :OrdrAmt :OrdrCcy :HldgsRedRate :Rndg :AcctId :SubAcctId :AgtCode :Grp1Or2Units :ReqdSttlmCcy :ReqdPrcCcy :ChrgTp :ChrgRt :ComssnTp :ComssnRt :PctgComssnWvd :SbLgOrdrRef :SbLgFndIdType :SbLgFndId :SbLgFndNm :SbLgPrdtCd :SbLgPctgOfTtlRedAmt :SbLgAcctId :SbLgSubAcctId :SbLgAgtCode :SbLgReqdSttlmCcy :SbLgReqdPrcCcy :SbLgChrgTp :SbLgChrgRt :SbLgComssnTp :SbLgComssnRt :SbLgPctgComssnWvd] v)))
-
-;; (defn parse-row [row]
-;;     (let [v (first (csv/parse-csv row))]
-;;       (zipmap [:OrdrTyp :DstId :FndPrvId :SwtchRef :OrdrRef :FndIdType :FndId :FndNm :PrdtCd :Unit :OrdrAmt :OrdrCcy :HldgsRedRate :Rndg :AcctId :SubAcctId :AgtCode :Grp1Or2Units :ReqdSttlmCcy :ReqdPrcCcy :ChrgTp :ChrgRt :ComssnTp :ComssnRt :PctgComssnWvd :SbLgOrdrRef :SbLgFndIdType :SbLgFndId :SbLgFndNm :SbLgPrdtCd :SbLgPctgOfTtlRedAmt :SbLgAcctId :SbLgSubAcctId :SbLgAgtCode :SbLgReqdSttlmCcy :SbLgReqdPrcCcy :SbLgChrgTp :SbLgChrgRt :SbLgComssnTp :SbLgComssnRt :SbLgPctgComssnWvd] v)))
-
-
-;; (defn- take-csv
-;;   "Takes file name and reads data."
-;;   [fname]
-;;   (with-open [file (java.io.BufferedReader.
-;;                  (java.io.FileReader. fname))]
-;;     ;; (doall (map (comp first csv/parse-csv) (line-seq file)))
-;;     ;; (csv/parse-csv (slurp file))
-;;     (doseq [line (line-seq file)]
-;;       (println (parse-row line))
-;;       )
-;;     ))
+(defn parse-row [header row]
+  (let [v (first (csv/parse-csv row))]
+    (zipmap header v)
+    )
+  )
 
 (defn take-csv
   "Takes file name and reads data."
   [fname]
   (with-open [file (java.io.BufferedReader.
                  (java.io.FileReader. fname))]
-    ;; (doall (map (comp first csv/parse-csv) (line-seq file)))
-    ;; (csv/parse-csv (slurp file))
-    (doseq [line (line-seq file)]
-      (println (parse-row line))
+    (let [header (first (csv/parse-csv (first (line-seq file))))]
+      (doall (map #(parse-row header %)  (line-seq file)))
       )
     ))
 
